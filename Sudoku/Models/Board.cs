d59e030e21018ff3acd7e63ff32666ac46384979
sfatmi <salman.fatmi@bglgroup.co.uk>
2021-05-12 11:57:30 +0100

    Add project files.

---
+++ Sudoku/Models/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku.Models
+{
+    public class Board
+    {
+        public List<Row> _rows { get; set; }
+    }
+
+    public class Row
+    {
+        public List<Square> _squares { get; set; }
+    }
+
+    public class Square
+    {
+        public int square { get; set; }
+    }
+}
---
+++ Sudoku/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sudoku.Models;
+
+namespace Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sudokuBoard = new List<List<int>>
+            {
+                new List<int>
+                {
+                    5,3,4,6,7,8,9,1,2
+                },
+                new List<int>
+                {
+                    5,3,4,6,7,8,9,1,2
+                }
+            };
+            var result = ValidateSolution(sudokuBoard);
+            Console.WriteLine(result);
+        }
+
+        private static bool ValidateSolution(List<List<int>> sudokuBoard) 
+        {
+            var correctedList = Enumerable.Range(1, 9).ToList();
+
+            foreach (var row in sudokuBoard)
+            {
+                var orderedRow = row.OrderBy(item => item);
+                var isEqual = Enumerable.SequenceEqual(correctedList, orderedRow);
+                if(!isEqual)
+                {
+                    return false;
+                }
+            }
+
+            var board = new Board();
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+
+                }
+            }
+
+            return true;
+        }
+    }
+}
